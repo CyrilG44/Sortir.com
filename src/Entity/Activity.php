ae0ffbe34c07b6c6a3fa7474c0fb719a620cc3a1
CyrilG44 <168413869+CyrilG44@users.noreply.github.com>
2024-08-26 16:47:48 +0200

    Business objects

---
+++ src/Entity/Activity.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActivityRepository::class)]
+class Activity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $starting_date = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $registration_limit_date = null;
+
+    #[ORM\Column]
+    private ?int $registration_max_nb = null;
+
+    #[ORM\Column(length: 500, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 250, nullable: true)]
+    private ?string $photo_url = null;
+
+    #[ORM\Column]
+    private ?bool $is_archived = null;
+
+    #[ORM\ManyToOne(inversedBy: 'activities')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $organizer = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'activities_participant')]
+    private Collection $participants;
+
+    #[ORM\ManyToOne(inversedBy: 'activities')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Place $place = null;
+
+    #[ORM\ManyToOne(inversedBy: 'activities')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?State $state = null;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartingDate(): ?\DateTimeInterface
+    {
+        return $this->starting_date;
+    }
+
+    public function setStartingDate(\DateTimeInterface $starting_date): static
+    {
+        $this->starting_date = $starting_date;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationLimitDate(): ?\DateTimeInterface
+    {
+        return $this->registration_limit_date;
+    }
+
+    public function setRegistrationLimitDate(\DateTimeInterface $registration_limit_date): static
+    {
+        $this->registration_limit_date = $registration_limit_date;
+
+        return $this;
+    }
+
+    public function getRegistrationMaxNb(): ?int
+    {
+        return $this->registration_max_nb;
+    }
+
+    public function setRegistrationMaxNb(int $registration_max_nb): static
+    {
+        $this->registration_max_nb = $registration_max_nb;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhotoUrl(): ?string
+    {
+        return $this->photo_url;
+    }
+
+    public function setPhotoUrl(?string $photo_url): static
+    {
+        $this->photo_url = $photo_url;
+
+        return $this;
+    }
+
+    public function isArchived(): ?bool
+    {
+        return $this->is_archived;
+    }
+
+    public function setArchived(bool $is_archived): static
+    {
+        $this->is_archived = $is_archived;
+
+        return $this;
+    }
+
+    public function getOrganizer(): ?User
+    {
+        return $this->organizer;
+    }
+
+    public function setOrganizer(?User $organizer): static
+    {
+        $this->organizer = $organizer;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(User $participant): static
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(User $participant): static
+    {
+        $this->participants->removeElement($participant);
+
+        return $this;
+    }
+
+    public function getPlace(): ?Place
+    {
+        return $this->place;
+    }
+
+    public function setPlace(?Place $place): static
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getState(): ?State
+    {
+        return $this->state;
+    }
+
+    public function setState(?State $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
