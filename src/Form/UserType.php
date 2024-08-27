4ecee78387a3322a76081625fef677665bff4e81
Dev-Zayrak <calle.florian@gmail.com>
2024-08-27 10:00:48 +0200

    Ajout connexion / remember me 1001 / 1002 - Florian

---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('roles')
+            ->add('password')
+            ->add('last_name')
+            ->add('first_name')
+            ->add('phone')
+            ->add('email')
+            ->add('is_admin')
+            ->add('is_active')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
