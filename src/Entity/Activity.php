9dc1ddfacd4fb62f2fafe2f2375f573334c0386d
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-08-27 13:55:08 +0200

    Ajout des clés uniques

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ActivityRepository::class)]
+#[ORM\UniqueConstraint(columns: ['name', 'starting_date', 'place', 'state'])]
+#[UniqueEntity(fields: ['name', 'starting_date', 'place', 'state'], message: "Une sortie identique est déjà proposée !")]
 class Activity
 {
     #[ORM\Id]
