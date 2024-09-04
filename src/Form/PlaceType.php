587f8e57850698237f47911755638480c128d18a
CyrilG44 <cyril.grignoux@gadz.org>
2024-09-04 17:08:25 +0200

    place crud [part 1]

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Place;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', null,[
+                'label' => 'Nom',
+            ])
+            ->add('street', null,[
+                'label' => 'Rue',
+            ])
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'label' => 'Ville',
+                'choice_label' => 'name',
+            ])
+            ->add('latitude',null, [
+                'attr' => ['readonly' => true],
+            ])
+            ->add('longitude',null, [
+                'attr' => ['readonly' => true],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Place::class,
+        ]);
+    }
+}
